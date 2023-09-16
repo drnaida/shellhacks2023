4ac9cd03bf895ba1521f7644696849b49894df2e
Timur Kalandarov <tkalandarov@usf.edu>
2023-09-16 11:04:42 -0400

    Add entities, extend data context

---
+++ shellhacks2023/Data/Exam.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace shellhacks2023.Data
+{
+    public class Exam
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+
+        [Required] 
+        public string Title { get; set; } = default!;
+
+        public User Owner { get; set; } = default!;
+        [ForeignKey(nameof(User))]
+        public Guid OwnerId { get; set; }
+
+        public List<Question> Questions { get; set; } = default!;
+    }
+}
