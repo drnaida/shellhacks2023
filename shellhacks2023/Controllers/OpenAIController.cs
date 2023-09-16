4241a93831aef1b465410a5fdafcf6179ed1838f
L42ARO <alvaro512@usf.edu>
2023-09-16 03:09:04 -0400

    feat: - added openai service and made it a singleton - Example of how to use it in openaicontroller

---
+++ shellhacks2023/Controllers/OpenAIController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using shellhacks2023.Services;
+
+namespace shellhacks2023.Controllers
+{
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using OpenAI_API;
+    using OpenAI_API.Completions;
+
+    namespace ChatGPT_App.Controllers
+    {
+        [Route("api/[controller]")]
+        [ApiController]
+        public class OpenAIController : ControllerBase
+        {
+            private readonly OpenAIService openAi;
+
+            public OpenAIController(OpenAIService openaiService)
+            {
+                this.openAi = openaiService;
+            }
+            [HttpGet]
+            [Route("TestTextCompletion")]
+            public async Task<IActionResult> TestTextCompletion(string query)
+            {
+                //Get a response from the OpenAI API
+                var response = await openAi.GetCompletionAsync(query);
+                //Return the response
+                return Ok(response);
+
+            }
+            [HttpGet]
+            [Route("TestChatCompletion")]
+            public async Task<IActionResult> TestChatCompletion(string query, string model)
+            {
+                //Get a response from the OpenAI API
+                var response = await openAi.GetChatCompletionAsync(query, model);  
+                //Return the response
+                return Ok(response);
+
+            }
+        }
+    }
+}
